2931004bfb7599fe1a00196b3337219d71f3868f
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-07-22 21:28:20 -0500

    conectar accounts con login

--- src/documex/urls.py
+++ src/documex/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name="index"),
-    path("loginUser",views.loginUser, name="loginUser")
+    path("",views.index, name="index")
 ]
 
