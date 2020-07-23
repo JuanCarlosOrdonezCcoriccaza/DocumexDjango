8cd161372302792c9f2934f88fe0a5a518dcd0df
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-07-23 16:01:06 -0500

    Version sostenible V1.5

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class administradores(models.Model):
+
+    username = models.CharField(max_length=50) 
+    password = models.CharField(min_length=8)
+    email    = models.EmailField(max_length=254)
+    img      = models.ImageField(upload_to='pics',null=False)
+
+
+class usuarios(models.Model):
+
+    first_name   = models.CharField(max_length=50)
+    last_name    = models.CharField(max_length=50)
+    username     = models.CharField(max_length=50) 
+    password     = models.CharField(min_length=8)    
+    email        = models.EmailField(max_length=254)
+    img          = models.ImageField(upload_to='pics',null=False)
+    
