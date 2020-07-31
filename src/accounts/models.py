f434cadaec16757e73dc88df288117dc06efd021
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-07-30 22:39:57 -0500

    clase Administrador

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -8,3 +8,12 @@ class Usuario(models.Model):
     apellido = models.CharField(max_length = 100)
     password = models.CharField(max_length = 100)
     correo = models.EmailField(max_length = 100)
+
+class Administrador(models.Model):
+    id = models.AutoField(primary_key= = True)
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    estado = models.BooleanField(default = False
+    correo = models.EmailField(max_length = 100)
+
