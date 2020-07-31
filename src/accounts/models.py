937076b7bf4ea3a3a457dcc71602e36f49a3b3aa
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-07-30 22:32:22 -0500

    clase usuario con campos provisionales

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 100);
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    correo = models.EmailField(max_length = 100)
