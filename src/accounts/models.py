50a73009437535e82c82fa1cc097031d527a72b3
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-07-31 17:24:03 -0500

    formulario campos correcciones

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -6,21 +6,24 @@ class Usuario(models.Model):
     id = models.AutoField(primary_key = True)
     nombre = models.CharField(max_length = 100)
     apellido = models.CharField(max_length = 100)
+    correo = models.EmailField(max_length = 100)
+    dni = models.IntegerField(max_length = 8)
     password = models.CharField(max_length = 100)
     fechaNacimiento = models.DateField()
     sexo = models.CharField(max_length=1)
     direccion = models.CharField(max_length=100)
-    dni = models.IntegerField(max_length = 8)
-    correo = models.EmailField(max_length = 100)
+    
+    
 
 class Administrador(models.Model):
     id = models.AutoField(primary_key= = True)
     nombre = models.CharField(max_length = 100)
     apellido = models.CharField(max_length = 100)
+    correo = models.EmailField(max_length = 100)
+    dni = models.IntegerField(max_length = 8)
     password = models.CharField(max_length = 100)
     estado = models.BooleanField(default = False)
-    dni = models.IntegerField(max_length = 8)
     sexo = models.CharField(max_length=1)
     fechaNacimiento = models.DateField()
     direccion = models.CharField(max_length=100)
-    correo = models.EmailField(max_length = 100)
+    
