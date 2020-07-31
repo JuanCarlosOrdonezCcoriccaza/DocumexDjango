9fd98f0b5e92ad7d0760f24de763f95108da5846
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-07-30 22:28:44 -0500

    clase usuario con clave primaria

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-    
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 100);
