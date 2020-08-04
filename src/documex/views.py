8c352a518ef79749c95ab99522fd57399f98c295
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-08-04 15:42:43 -0500

    modelo arreglado

--- src/documex/views.py
+++ src/documex/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Documento
 
 # Create your views here.
 def index(request):
@@ -16,3 +17,9 @@ def formSubir(request):
 
 def nuevoDocumento(request):
     pass
+
+def listarDocumentos(request):
+    
+    docs = Documento.objects.all() 
+
+    return render(request,"misDocumentos.html",{'docs':docs})
