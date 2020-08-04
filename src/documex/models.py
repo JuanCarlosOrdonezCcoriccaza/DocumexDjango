1f911a7056286eb0641401e2622cbd8bb6f24a73
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-08-04 13:42:35 -0500

    modelo documento

--- src/documex/models.py
+++ src/documex/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from accounts.models import Administrador,Usuario
 # Create your models here.
 class Documento (models.Model):
     id           = models.AutoField(primary_key = True)
-    dniautor     = models.IntegerField(models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE))
+    autor        = models.ForeignKey(verbose_name="Usuario")  
     archivo      = models.FileField(upload_to="Archivos",null=False)
     titulo       = models.TextField(max_length=100)
     descripcion  = models.TextField(max_length=150)
@@ -11,3 +12,4 @@ class Documento (models.Model):
     enviado      = models.BooleanField(default=False)     
     recibido     = models.BooleanField(default=False)
     costo        = models.FloatField()
+    #ubicacion
