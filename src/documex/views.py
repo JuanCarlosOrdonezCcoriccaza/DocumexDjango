269167f43548a009cbf1d184d5421b932bcfe891
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-08-05 02:25:09 -0500

    documex views formSubir

--- src/documex/views.py
+++ src/documex/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Documento
-
+from accounts.models import Administrador,Usuario
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -10,16 +12,31 @@ def herramienta(request):
     return render(request,"herramienta.html")
 
 def misDocumentos(request):
-    return render(request,"herramientas/misDocumentos.html")
-
-def formSubir(request):
-    return render(request,"herramientas/formSubir.html")
+    docs = Documento.objects.all() 
+    print(docs)
+    return render(request,"herramientas/misDocumentos.html",{'docs':docs})
 
 def nuevoDocumento(request):
+    #return render(request,"herramientas/formSubir.html")
     pass
 
-def listarDocumentos(request):
-    
-    docs = Documento.objects.all() 
+def formSubir(request):
+    if request.method=='POST':
+        archivo=request.FILES['archivo']
+        titulo=request.POST['titulo']
+        descripcion=request.POST['descripcion']
+        autor=request.user
+        documento = Documento.objects.create(autor=autor,archivo=archivo,titulo=titulo,descripcion=descripcion)
+        documento.save()
+        print("archivo ",archivo)
+        print("titulo",titulo)
+        print("descripcion",descripcion)
+        print("usuario",request.user)
+        print("guardado")
+        messages.info(request,'Documento guardado')
+        #docs=Documento.objects.all()
+        return redirect('/')
+    else:
+        return render(request,'herramientas/formSubir.html')
+      
 
-    return render(request,"misDocumentos.html",{'docs':docs})
