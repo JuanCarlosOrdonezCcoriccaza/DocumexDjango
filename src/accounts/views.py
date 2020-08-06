7da44ac428173c6eca6cdf1d5776847b08d799f8
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-08-06 13:57:14 -0500

    vista listar usuarios

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -141,6 +141,12 @@ def editarUsuario(request,id):
     #usuario = Usuario.objects.get(id = id)
     #if request.method == 'GET':
         
+def listarUsuarios(request):
+    usuarios = Usuario.objects.all()
+    contexto ={
+        'usuarios' : usuarios
+    }
+    return render(request,'listarUsers.html',contexto)       
 
 def listarUsuarios(request):
     usuarios = Usuario.objects.all()
